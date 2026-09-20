133b5e16130ef1e5ca78cccd8aa2f2589182869d
LKTech Admin <lktech@example.com>
2026-09-20 23:34:26 +0700

    Ubah layout logo metode pembayaran di mobile kembali menjadi flex-wrap agar tidak memakan tempat

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -99,10 +99,10 @@
             </div>
 
             <!-- Kolom 3: Metode Pembayaran & Sosial Media -->
-            <div class="self-start">
+            <div class="self-start w-full">
                 <!-- Metode Pembayaran -->
                 <h4 class="font-bold text-gray-800 mb-3 font-montserrat flex items-center max-md:text-[1.1rem]">Metode Pembayaran</h4>
-                <div class="grid grid-cols-2 gap-2 mb-4">
+                <div class="flex flex-wrap md:grid md:grid-cols-2 gap-2 mb-4">
                     
                     <div class="px-2.5 py-1 bg-white border border-gray-200 shadow-sm rounded text-[11px] font-black flex items-center justify-center tracking-tighter">
                         <span class="text-[#005E6A]">Livin'</span> <span class="text-[#FFB71B] ml-0.5">Mandiri</span>
